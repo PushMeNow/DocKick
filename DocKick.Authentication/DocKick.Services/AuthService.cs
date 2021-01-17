8aeba222c6edf1eac4d5caa1ce207989ccd20670
kborovikforte <konstantin.borovik@fortegrp.com>
2021-01-17 12:14:41 +0300

    - remove old logic for jwt token generating.

--- DocKick.Authentication/DocKick.Services/AuthService.cs
+++ DocKick.Authentication/DocKick.Services/AuthService.cs
@@ -1,9 +1,7 @@
-﻿using System.Security.Claims;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using DocKick.DataTransferModels.User;
 using DocKick.Entities.Users;
 using DocKick.Exceptions;
-using Google.Apis.Auth;
 using IdentityServer4.Events;
 using IdentityServer4.Services;
 using Microsoft.AspNetCore.Identity;
@@ -17,43 +15,17 @@ namespace DocKick.Services
         private readonly IIdentityServerInteractionService _interaction;
 
         private readonly SignInManager<User> _signInManager;
-        private readonly ITokenService _tokenService;
         private readonly UserManager<User> _userManager;
 
         public AuthService(UserManager<User> userManager,
                            SignInManager<User> signInManager,
-                           ITokenService tokenService,
                            IIdentityServerInteractionService interaction,
                            IEventService events)
         {
             _userManager = userManager;
             _signInManager = signInManager;
-            _tokenService = tokenService;
             _interaction = interaction;
             _events = events;
-        }
-
-        public async Task<AuthenticatedUserResult> Authenticate(string token)
-        {
-            ExceptionHelper.ThrowArgumentNullIfNull(token, nameof(token));
-
-            var payload = await GoogleJsonWebSignature.ValidateAsync(token, new GoogleJsonWebSignature.ValidationSettings());
-            var user = await _userManager.FindByEmailAsync(payload.Email);
-
-            if (user is null)
-            {
-                user = new User
-                       {
-                           Email = payload.Email,
-                           UserName = payload.Email
-                       };
-
-                var result = await _userManager.CreateAsync(user);
-
-                ExceptionHelper.ThrowIfTrue<AuthenticationException>(!result.Succeeded);
-            }
-
-            return await GetAuthenticatedUserResultAndAddRefreshToken(user, true);
         }
 
         public async Task<bool> Authenticate(InternalUserAuthModel model)
@@ -95,64 +67,26 @@ namespace DocKick.Services
 
         public async Task<AuthenticatedUserResult> SignUp(SignUpModel model)
         {
-            ExceptionHelper.ThrowArgumentNullIfNull(model, nameof(model));
-
-            var checkUser = await _userManager.FindByEmailAsync(model.Email);
-
-            ExceptionHelper.ThrowIfNotNull<AuthenticationException>(checkUser);
-
-            var user = new User
-                       {
-                           Email = model.Email,
-                           UserName = model.Email
-                       };
-
-            var createResult = await _userManager.CreateAsync(user, model.Password);
-
-            ExceptionHelper.ThrowIfTrue<AuthenticationException>(!createResult.Succeeded);
-
-            await _userManager.AddClaimAsync(user, new Claim("email", user.Email));
-
-            return await GetAuthenticatedUserResultAndAddRefreshToken(user);
-        }
-
-        public async Task<AuthenticatedUserResult> RefreshToken(RefreshTokenModel model)
-        {
-            var userId = await _tokenService.GetUserIdFromAccessToken(model.AccessToken);
-            var user = await _userManager.FindByIdAsync(userId.ToString());
-
-            ExceptionHelper.ThrowNotFoundIfNull(user, "User");
-            ExceptionHelper.ThrowTokenExceptionIfTrue(!await _tokenService.ValidateRefreshToken(user, model.RefreshToken), "Refresh token is invalid.");
-
-            await _tokenService.DeleteRefreshToken(model.RefreshToken);
-
-            return await GetAuthenticatedUserResultAndAddRefreshToken(user);
-        }
-
-        private async Task<AuthenticatedUserResult> GetAuthenticatedUserResult(User user)
-        {
-            var authenticatedUser = new AuthenticatedUserResult
-                                    {
-                                        Email = user.Email,
-                                        AccessToken = await _tokenService.GenerateAccessToken(user),
-                                        RefreshToken = await _tokenService.GenerateRefreshToken()
-                                    };
-
-            return authenticatedUser;
-        }
-
-        private async Task<AuthenticatedUserResult> GetAuthenticatedUserResultAndAddRefreshToken(User user, bool clearRefreshTokens = false)
-        {
-            var result = await GetAuthenticatedUserResult(user);
-
-            if (clearRefreshTokens)
-            {
-                await _tokenService.ClearUserRefreshTokens(user);
-            }
-
-            await _tokenService.CreateRefreshToken(user, result.RefreshToken);
-
-            return result;
+            // ExceptionHelper.ThrowArgumentNullIfNull(model, nameof(model));
+            //
+            // var checkUser = await _userManager.FindByEmailAsync(model.Email);
+            //
+            // ExceptionHelper.ThrowIfNotNull<AuthenticationException>(checkUser);
+            //
+            // var user = new User
+            //            {
+            //                Email = model.Email,
+            //                UserName = model.Email
+            //            };
+            //
+            // var createResult = await _userManager.CreateAsync(user, model.Password);
+            //
+            // ExceptionHelper.ThrowIfTrue<AuthenticationException>(!createResult.Succeeded);
+            //
+            // await _userManager.AddClaimAsync(user, new Claim("email", user.Email));
+            //
+            // return await GetAuthenticatedUserResultAndAddRefreshToken(user);
+            return null;
         }
     }
 }
--- DocKick.Authentication/DocKick.Services/IAuthService.cs
+++ DocKick.Authentication/DocKick.Services/IAuthService.cs
@@ -6,13 +6,10 @@ namespace DocKick.Services
 {
     public interface IAuthService
     {
-        Task<AuthenticatedUserResult> Authenticate(string token);
-
         Task<bool> Authenticate(InternalUserAuthModel model);
 
         Task<AuthenticatedUserResult> SignUp(SignUpModel model);
 
-        Task<AuthenticatedUserResult> RefreshToken(RefreshTokenModel model);
         Task<string> Logout(string logoutId, string subjectId, string displayName);
     }
 }

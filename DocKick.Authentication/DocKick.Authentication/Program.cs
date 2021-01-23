2cb81e5a490f380ba14337e97783c72f1b30c7c2
Konstantin Borovik <konstantin.borovik@fortegrp.com>
2021-01-23 22:17:42 +0300

    - add docker for auth service

--- DocKick.Authentication/DocKick.Authentication/Program.cs
+++ DocKick.Authentication/DocKick.Authentication/Program.cs
@@ -1,15 +1,26 @@
+using System;
+using System.Threading.Tasks;
+using DocKick.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace DocKick.Authentication
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args)
-                .Build()
-                .Run();
+            var host = CreateHostBuilder(args)
+                .Build();
+#if DOCKER
+            await EnsureDatabaseCreatedAsync(host);
+#endif
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
@@ -17,9 +28,34 @@ namespace DocKick.Authentication
             return Host.CreateDefaultBuilder(args)
                        .ConfigureWebHostDefaults(webBuilder =>
                                                  {
-                                                     webBuilder.UseStartup<Startup>();
-                                                     webBuilder.UseWebRoot("wwwroot");
+                                                     webBuilder.UseStartup<Startup>()
+                                                               .UseWebRoot("wwwroot");
+
+#if DOCKER
+                                                     webBuilder.ConfigureAppConfiguration(builder =>
+                                                                                          {
+                                                                                              builder.AddJsonFile("appsettings.Docker.json");
+                                                                                          });
+#endif
                                                  });
+        }
+
+        private static async Task EnsureDatabaseCreatedAsync(IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                var context = services.GetRequiredService<DocKickAuthDbContext>();
+
+                await context.Database.MigrateAsync();
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex.Message, "An error occurred seeding the DB.");
+            }
         }
     }
 }

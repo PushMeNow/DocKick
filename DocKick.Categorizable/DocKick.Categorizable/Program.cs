415505c0988fbb0598f8eaac18db7ccebc80b2ff
Konstantin <kes8594@yandex.ru>
2021-03-08 18:10:30 +0300

    - add docker files and configs for categorizable proj

--- DocKick.Categorizable/DocKick.Categorizable/Program.cs
+++ DocKick.Categorizable/DocKick.Categorizable/Program.cs
@@ -1,28 +1,72 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+
+#if DOCKER
+
+using System;
+using DocKick.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+
+#endif
 
 namespace DocKick.Categorizable
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args)
-                .Build()
-                .Run();
+            var host = CreateHostBuilder(args)
+                .Build();
+
+            #if DOCKER
+
+            await EnsureDatabaseCreatedAsync(host);
+
+            #endif
+
+            await host.RunAsync();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                                          {
-                                              webBuilder.UseStartup<Startup>();
-                                          });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                       .ConfigureWebHostDefaults(webBuilder =>
+                                                 {
+                                                     webBuilder.UseStartup<Startup>();
+
+                                                     #if DOCKER
+                                                     webBuilder.ConfigureAppConfiguration(builder =>
+                                                                                          {
+                                                                                              builder.AddJsonFile("appsettings.Docker.json");
+                                                                                          });
+                                                     #endif
+                                                 });
+        }
+
+        #if DOCKER
+
+        private static async Task EnsureDatabaseCreatedAsync(IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                var context = services.GetRequiredService<CategorizableDbContext>();
+
+                await context.Database.MigrateAsync();
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex.Message, "An error occurred seeding the DB.");
+            }
+        }
+
+        #endif
     }
 }
